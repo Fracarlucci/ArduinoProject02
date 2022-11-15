475fb5cc3dce5db1ba37a0668a824040602b803e
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2022-11-15 15:39:06 +0100

    Implemented photoresistor

---
+++ src/devices/Photoresistor.h
@@ -0,0 +1,21 @@
+#ifndef __PHOTORESISTOR__
+#define __PHOTORESISTOR__
+
+#include "Arduino.h"
+
+// define the level of threshold
+#define THl 10
+
+// define costant to get the luminosity
+#define PHOTORES_PIN A0
+#define FREQ 5
+#define PERIOD 1000/FREQ
+
+class Photoresistor {
+
+public:
+  double readLuminosity();
+  bool isLuminosityHigher();  
+};
+
+#endif  
