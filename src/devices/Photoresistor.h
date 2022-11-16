16bdc09f5a25734170ebf81ec953c23fda9f0eae
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2022-11-16 09:41:44 +0100

    add photoresistor constructor

--- src/devices/Photoresistor.h
+++ src/devices/Photoresistor.h
@@ -3,20 +3,21 @@
 
 #include "Arduino.h"
 
-// define the level of threshold
-#define THl 10
-
 // define costant to get the luminosity
-#define PHOTORES_PIN A0
 #define FREQ 5
 #define PERIOD 1000/FREQ
 
 class Photoresistor {
 
-public:
-  Photoresistor(const int photoresPin, const int lightThreshold);
-  double readLuminosity();
-  bool isLuminosityHigher();  
+  public:
+    Photoresistor(const int photoresPin, const double lightThreshold);
+    double readLuminosity();
+    bool isLuminosityHigher();
+
+  private:
+    int pin;
+    double THl;
+
 };
 
 #endif  
