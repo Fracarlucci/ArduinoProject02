da1ea68f28c09da4508c3fb73cd157d107aaf54f
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2022-11-21 16:55:34 +0100

    Add test for WaterLevelTask

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "scheduler/Scheduler.h"
 #include "tasks/SmartLighting.h"
+#include "tasks/WaterLevelTask.h"
 
 Scheduler sched;
 
@@ -9,12 +10,14 @@ void setup() {
   Serial.begin(9600);
   sched.init(100);
  
-  Task* t0 = new SmartLighting(8, 9, A0, 3, 10);
-  //Task* t1 = new WaterLevelTask();
+  //Task* t0 = new SmartLighting(8, 9, A0, 3, 10);
+  WaterLevelTask* t1 = new WaterLevelTask(1, 0, 99, 100, 2, 3, 88, 11);
  
-  t0->init(100);
+  //t0->init(100);
+  t1->init(100, 500);
   
-  sched.addTask(t0);
+  //sched.addTask(t0);
+  sched.addTask(t1);
 }
 
 void loop() {
